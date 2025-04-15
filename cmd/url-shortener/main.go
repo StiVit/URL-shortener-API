15ad105a23578b233adf1ec74c217e07fad27f74
vitalie stinca <vitalicstinca123@gmail.com>
2025-04-15 17:44:21 +0100

    feat: init server

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"log/slog"
+	"net/http"
 	"os"
 
 	"github.com/StiVit/URL-shortener-API/internal/config"
@@ -52,10 +53,26 @@ func main() {
 	router.Use(middleware.Logger)
 	router.Use(logger.New(log))
 	router.Use(middleware.Recoverer) // If the handler panics, this recovers and writes a 500
-	router.Use(middleware.URLFormat) // If the URL is not valid, this middleware will return a 400 Bad Request
-
-	router.Post("/url", save.New(log, storage))
+	router.Use(middleware.URLFormat) // If the URL is not v alid, this middleware will return a 400 Bad Request
+ 
+	router.Post("/url", save.New(log, storage)) 
 	// TODO: init server
+
+	log.Info("starting server", slog.String("address", config.Address))
+
+	srv := &http.Server{
+		Addr: config.Address,
+		Handler: router, // Even if router uses handlers to connect and process requests, the router by itself is s handler
+		ReadTimeout: config.HTTPServer.Timeout,
+		WriteTimeout: config.HTTPServer.Timeout,
+		IdleTimeout: config.HTTPServer.IdleTimeout,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("Failed to start server")
+	}
+
+	log.Error("Server Stopped")
 
 }
 
